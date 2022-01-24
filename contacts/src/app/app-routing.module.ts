151994621b57418b00d781fedb46722f6a8dd5a6
Meera <meera@foundingminds.com>
2022-01-25 00:30:14 +0530

    Contacts module added, routing file changed

--- contacts/src/app/app-routing.module.ts
+++ contacts/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'contacts', pathMatch: 'full'},
+  { path: 'contacts', loadChildren: () => import('./Modules/contacts/contacts.module').then(m => m.ContactsModule) },
+  { path: '**', redirectTo: 'contacts'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
