8a83e6541b0d47b6d41689e5db9a9a7410b3f49e
Meera <meera@foundingminds.com>
2022-01-25 02:16:02 +0530

    list and create contact template

--- contacts/src/app/modules/home/contacts/contacts.component.ts
+++ contacts/src/app/modules/home/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contacts',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  newContact(){
+    this.router.navigate(['/create']);
   }
 
 }
---
+++ contacts/src/app/modules/home/contacts/create-or-edit-contact/create-or-edit-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-or-edit-contact',
+  templateUrl: './create-or-edit-contact.component.html',
+  styleUrls: ['./create-or-edit-contact.component.css']
+})
+export class CreateOrEditContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
